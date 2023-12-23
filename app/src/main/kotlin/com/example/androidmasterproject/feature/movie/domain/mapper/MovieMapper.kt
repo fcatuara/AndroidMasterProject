78eaf771c5c58254093228821af0a0c0003ca56d
Flavio Catuara <f.catuara@outlook.it>
2023-12-23 19:21:47 +0100

    fix: import

--- app/src/main/kotlin/com/example/androidmasterproject/feature/movie/domain/mapper/MovieMapper.kt
+++ app/src/main/kotlin/com/example/androidmasterproject/feature/movie/domain/mapper/MovieMapper.kt
@@ -1,6 +1,7 @@
 package com.example.androidmasterproject.feature.movie.domain.mapper
 
 import com.example.androidmasterproject.core.mapper.Mapper
+import com.example.androidmasterproject.feature.movie.data.remote.dto.MovieDto
 import com.example.androidmasterproject.feature.movie.domain.model.Movie
 
 class MovieMapper : Mapper<MovieDto, Movie> {
